a91af5c5bdd34e2e617f97a8766829068e11e2f0
KialRyan <kleiteryan@mail.csuchico.edu>
2018-02-23 20:01:25 -0800

    Doesn't loop

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// Kial Leite Ryan
 #include<iostream>
 #include<string>
 using namespace std;
@@ -7,13 +8,38 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-
+  string itemInput;
+  bool myFlag = false;
+  
+  do
+  {
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
-  cin>>input;
+  cin>> input;
 
+    if (input == 'Q' || input == 'q')
+    {
+       myFlag = true;
+    }
+
+    else if (input == 'A' || input == 'a')
+    {
+        myFlag = true;
+        cout <<"What is the item?"<< endl;
+        cin >> itemInput;
+
+        if (numItems >=5 || numItems < 0)
+            cout << "You'll need a bigger list" << endl;
+
+        else if (numItems < 5)
+        {
+            list[numItems] = itemInput;
+            numItems++;
+        }
+      }
+   } while (!myFlag);  
   return 0;
 }
